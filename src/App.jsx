97d0c37598ee578a5b32a398aa983ffe20ce8fd1
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-08-08 17:29:43 +0700

    Expression Conditionals in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 export default class App extends Component {
+  state = {
+    name: 'ndhung',
+    loading: false,
+    showName: true,
+  };
+
   render() {
+    const { name, loading, showName } = this.state;
     return (
       <div className='App'>
-        <h1>Hello world</h1>
+        {loading ? <h4>Loading...</h4> : <h4>Hello {showName && name}</h4>}
+        <div className='action'>
+          <button  className='btn btn-outline-primary' onClick={() => this.setState({ loading: !loading })}>
+            Toggle loading
+          </button>
+          <button className='btn btn-outline-primary' onClick={() => this.setState({ showName: !showName })}>
+            Toggle show Name
+          </button>
+        </div>
       </div>
-    )
+    );
   }
 }
