65ed80595e4ff62bcb2751b802983bfec84f617d
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-08-08 17:04:40 +0700

    Clean up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
